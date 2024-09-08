8fe140644157ccd53f8c915ae35ebb259fdc44d1
b-hayes <bradozman@gmail.com>
2024-09-08 17:46:39 +1000

    Let the terminal window close after running the bat file

--- arena/ra3_server.bat
+++ arena/ra3_server.bat
@@ -3,4 +3,4 @@
 // Script should be located and run from your /quake3/arena directory
 
 cd ..
-quake3.exe +set fs_game arena +set vm_game 0 +set sv_pure 0 +set bot_enable 0 +set sv_punkbuster 0 +set dedicated 2 +set net_port 27960 +exec server.cfg
+start quake3.exe +set fs_game arena +set vm_game 0 +set sv_pure 0 +set bot_enable 0 +set sv_punkbuster 0 +set dedicated 2 +set net_port 27960 +exec server.cfg
